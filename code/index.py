586c163dd47314267b9f5ee2c6b086240b6da154
Weijie <52730904+simweijie@users.noreply.github.com>
2021-04-18 23:17:22 +0800

    added sms capability, changed to us east 1 region

--- code/index.py
+++ code/index.py
@@ -3,6 +3,7 @@ import logging
 import pymysql
 import json
 import os
+import boto3
 
 #rds settings
 rds_endpoint = os.environ['rds_endpoint']
@@ -12,6 +13,12 @@ db_name=os.environ['db_name']
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
+# Initialize SNS client for Singapore Region
+session = boto3.Session(
+    region_name="us-east-1"
+)
+sns_client = session.client('sns')
 
 #Connection
 try:
@@ -25,22 +32,53 @@ logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
 
 def handler(event, context):
     cur = connection.cursor()  
-## Retrieve Data    
+## Update queue status    
     query = "UPDATE Queue SET status='{}' where branchId='{}' and customerId='{}' and status='{}'".format(event['newStatus'],event['branchId'],event['customerId'],event['currentStatus'])
     cur.execute(query)
     connection.commit()
-    print(cur.rowcount, "record(s) affected")
+    print(cur.rowcount, "record(s) update statements affected")
+
+## Retrieve contact details of customer next to see doctor
+    if event['newStatus']=='D':
+        print('lol')
+        query="SELECT c.name,c.contactNo,q.queueNumber \
+            FROM Customer c, Queue q \
+            WHERE q.branchId='{}' AND q.status='Q' AND c.id=q.customerId \
+            ORDER BY queueNumber ASC LIMIT 1".format(event['branchId'])
+        cur.execute(query)
+        connection.commit()
+        print(cur.rowcount, "record(s) select statements affected")
+        rows = cur.fetchall()
+        contactNo= ""
+        name=""
+        for row in rows:
+            print("TEST {0} {1} {2}".format(row[0],row[1],row[2]))
+            name = row[0]
+            contactNo = row[1]
+
+## Prepare and send SMS
+        ## https://blog.shikisoft.com/send-sms-with-sns-aws-lambda-python/
+        print(contactNo)
+        response = sns_client.publish(
+            PhoneNumber='+65' + contactNo,
+            Message='Hi ' + name + 
+            ', please note that you are next in line to see the doctor.',
+            MessageAttributes={
+                'AWS.SNS.SMS.SenderID': {
+                    'DataType': 'String',
+                    'StringValue': 'eQueue'
+                },
+                'AWS.SNS.SMS.SMSType': {
+                    'DataType': 'String',
+                    'StringValue': 'Promotional'
+                }
+            }
+        )
+
 ## Construct body of the response object
     transactionResponse = {}
 # Construct http response object
     responseObject = {}
-    # responseObject['statusCode'] = 200
-    # responseObject['headers'] = {}
-    # responseObject['headers']['Content-Type']='application/json'
-    # responseObject['headers']['Access-Control-Allow-Origin']='*'
     responseObject['data'] = json.dumps(transactionResponse, sort_keys=True,default=str)
-    
-    #k = json.loads(responseObject['body'])
-    #print(k['uin'])
 
     return responseObject
